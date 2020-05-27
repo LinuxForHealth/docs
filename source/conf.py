dc698f145f3019448b347f98c915ab5da18faead
ccorley <ccorley@us.ibm.com>
2020-05-27 09:29:54 -0500

    cleanup

--- source/conf.py
+++ source/conf.py
@@ -66,7 +66,7 @@ html_theme_options = {
     'logo_only': False,
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
+    'style_external_links': True,
     'style_nav_header_background': '0x4380B4',
     # Toc options
     'collapse_navigation': True,
